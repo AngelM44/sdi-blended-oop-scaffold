271cea50f5c1f256cd55cd4732fd8d721f0d2920
FAngel2 <fernandoa2128@gmail.com>
2023-07-24 09:22:07 -0700

    V1

--- index.js
+++ index.js
@@ -2,13 +2,56 @@ class Member {
   constructor(name) {}
 }
 class BankAccount {
-  constructor(member) {}
-  credit() {}
-  debit() {}
-  checkBalance() {}
+  #balance 
+  #transactions;
+
+  constructor(balance) {
+    this.#balance = 0;
+    this.#transactions = [];
+  }
+  credit(addMoney) {
+    this.#balance  += addMoney;
+    this.#transactions.push(addMoney);
+  }
+
+  debit(takeMoney) {
+    this.#balance -= takeMoney;
+    this.#transactions.push(takeMoney);
+  }
+
+  checkBalance() {
+    return `Your current balance is: ${this.#balance} `
+  }
+
+  get getBalance() {
+    return this.#balance;
+  }
+  set setBalance(newBalance){
+    this.#balance = newBalance;
+  }
+  static transactionHistory(accountHistory){
+    if ( accountHistory instanceof BankAccount) {
+    return accountHistory.#transactions;
+    }else{
+      return console.log("sorry, not found")
+    }
+
+  }
+
 }
-class CheckingAccount {
-  constructor(member) {}
+class CheckingAccount extends BankAccount {
+  #balance
+  
+  constructor(balance) {
+    super (balance)
+  }
+  debit(takeMoney){
+    if((this.#balance - takeMoney) < 0){
+    return "You have insufficient funds to perform that action";
+    }else{
+      this.#balance -= takeMoney;
+    }
+  }
 }
 class SavingsAccount {
   constructor(member) {}
